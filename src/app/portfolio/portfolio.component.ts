2d30720ef39c217598ccd8b11a73f49074c50108
Mahmoud-Jamal <mahmoud.gemy9966@gmail.com>
2025-01-11 16:14:59 +0200

    new commit

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  images = [
+    "port1.png",
+    "port2.png",
+    "port3.png",
+    "port1.png",
+    "port2.png",
+    "port3.png"
+  ]
+  image:string=""
+  changeImag(item:string):void{
+    this.image=item
+
+  }
+
 }
